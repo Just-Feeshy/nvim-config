03a8d44ac3634c6937615c03056dd1c86e07a5c9
Diego Fonseca <fonseca.diego.feeshy@gmail.com>
2026-01-24 09:38:52 -0500

    nightfox configured!

--- lua/modules/nightfox.lua
+++ lua/modules/nightfox.lua
@@ -1,1 +1,11 @@
-return { "EdenEast/nightfox.nvim" }
+return {
+  "EdenEast/nightfox.nvim",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    vim.o.termguicolors = true
+    require("nightfox").setup({})
+    vim.cmd("colorscheme carbonfox")
+    vim.api.nvim_set_hl(0, "Visual", { bg = "#2b2f36" })
+  end,
+}
